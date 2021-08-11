2bbe3a06c057d696e135c2da94c1ff9c21d2df7a
Amuhari <75573479+Amuhari@users.noreply.github.com>
2021-08-11 15:46:42 +0000

    Add files via upload

---
+++ AzureVMSKU.ps1
@@ -0,0 +1,10 @@
+﻿$location="CentralUS"
+$PublisherNmae="MicrosoftWindowsDesktop"
+$Offer="office-365"
+$SkuMatchString="*evd*"
+
+ 
+
+$sku= Get-AzVMImageSku -Location $location -PublisherName $PublisherNmae -Offer $Offer|? {$_.Skus -like $SkuMatchString}
+$sku|Get-AzVMImage|Sort-Object -Descending -Property Version|select version,Skus,offer,PublisherName
+ 
